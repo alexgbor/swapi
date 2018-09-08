3335fdada300b00aac810a56783972e378f8a1f9
Alex GB | Link_Z <alexgomezborrego@gmail.com>
2018-09-08 21:12:54 +0200

    added second solution to getAllPeople, need to choose one

--- logic/index.spec.js
+++ logic/index.spec.js
@@ -12,12 +12,30 @@ describe('business logic', () => {
 
 
 
-        it('should succeed', () => 
+        it('should succeed', () =>
             logic.getAllPeople()
                 .then(res => {
                     expect(res).to.exist
                     expect(res.length).not.to.equal(0)
-                    expect(Object.keys(res[Math.round(res.length*Math.random())]).length).to.equal(16)
+                    expect(Object.keys(res[Math.round(res.length * Math.random())]).length).to.equal(16)
+                })
+        )
+
+        false && it('should fail on non-string url', () =>
+            logic.getAllPeople(123)
+                .catch(err => err)
+                .then(message => {
+                    expect(message).to.exist
+                    expect(message).to.equal('invalid url')
+                })
+        )
+
+        false && it('should fail on different url', () =>
+            logic.getAllPeople('http://google.es')
+                .catch(err => err)
+                .then(message => {
+                    expect(message).to.exist
+                    expect(message).to.equal('invalid url')
                 })
         )
     })
